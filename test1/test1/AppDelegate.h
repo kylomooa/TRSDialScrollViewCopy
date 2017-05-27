1b0a1aaf02859e84b180951344aa62d67d9e1361
maoqiang <kylo.mooa@gmail.com>
2017-05-27 11:26:21 +0800

    edit
    edit

---
+++ test1/test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by 毛强 on 2016/11/3.
+//  Copyright © 2016年 maoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test1
+//
+//  Created by 毛强 on 2016/11/3.
+//  Copyright © 2016年 maoqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
