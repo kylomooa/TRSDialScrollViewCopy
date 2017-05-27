64e9c4fd6ff81e45162556a9954d43c8e5f452ab
maoqiang <kylo.mooa@gmail.com>
2017-05-27 10:55:24 +0800

    +add
    +add

---
+++ TRSDialScrollView/TRSDialScrollView/TRSDialView.h
@@ -0,0 +1,134 @@
+/***************************************************
+ *  ____              ___   ____                   *
+ * |  _ \ __ _ _ __  ( _ ) / ___|  ___ __ _ _ __   *
+ * | |_) / _` | '_ \ / _ \/\___ \ / __/ _` | '_ \  *
+ * |  __/ (_| | | | | (_>  <___) | (_| (_| | | | | *
+ * |_|   \__,_|_| |_|\___/\/____/ \___\__,_|_| |_| *
+ *                                                 *
+ ***************************************************/
+
+#import <UIKit/UIKit.h>
+
+typedef enum : NSUInteger {
+    RulerEnumValueBG = 0,
+    RulerEnumValueBMI,
+    RulerEnumValueOther,
+} RulerEnum;
+
+
+@interface TRSDialView : UIView <UIAppearance>
+
+//添加一个类型用来区分血糖、BMI类型
+@property (nonatomic, assign)RulerEnum RulerEnumType;
+
+
+#pragma mark - Methods
+
+/**
+ * Method to set the range of values to display
+ */
+- (void)setDialRangeFrom:(CGFloat)from to:(CGFloat)to;
+
+#pragma mark - Dial Properties
+
+@property (assign, readonly, nonatomic) NSInteger leading;
+
+/**
+ * The maximum value to display in the dial
+ */
+@property (assign, readonly, nonatomic) NSInteger minimum;
+
+/**
+ * The minimum value to display in the dial
+ */
+@property (assign, readonly, nonatomic) CGFloat maximum;
+
+/**
+ * The number of minor ticks per major tick
+ */
+@property (assign, nonatomic) NSInteger minorTicksPerMajorTick;
+
+/**
+ * The number of pixels/points between minor ticks
+ */
+@property (assign, nonatomic) NSInteger minorTickDistance;
+
+/**
+ * The image to use as the background image
+ */
+@property (strong, nonatomic) UIColor *backgroundColor;
+
+#pragma mark - Tick Label Properties
+
+/**
+ * The tick label stroke color
+ */
+@property (strong, nonatomic) UIColor *labelStrokeColor;
+
+/**
+ * The width of the stroke line used to trace the Label text
+ */
+@property (assign, nonatomic) CGFloat labelStrokeWidth;
+
+/**
+ * The tick label fill color
+ */
+@property (strong, nonatomic) UIColor *labelFillColor;
+
+/**
+ * The tick label font
+ */
+@property (strong, nonatomic) UIFont *labelFont;
+
+#pragma mark - Minor Tick Properties
+
+/**
+ * The minor tick color
+ */
+@property (strong, nonatomic) UIColor *minorTickColor;
+
+/**
+ * The length of the minor ticks
+ */
+@property (assign, nonatomic) CGFloat minorTickLength;
+
+/**
+ * The length of the Major Tick
+ */
+@property (assign, nonatomic) CGFloat minorTickWidth;
+
+#pragma mark - Major Tick Properties
+
+/**
+ * The color of the Major Tick
+ */
+@property (strong, nonatomic) UIColor *majorTickColor;
+
+/**
+ * The length of the Major Tick
+ */
+@property (assign, nonatomic) CGFloat majorTickLength;
+
+/**
+ * The width of the Major Tick
+ */
+@property (assign, nonatomic) CGFloat majorTickWidth;
+
+#pragma mark - Shadow Properties
+
+/**
+ * The shadow color
+ */
+@property (strong, nonatomic) UIColor *shadowColor;
+
+/**
+ * The shadow offset
+ */
+@property (assign, nonatomic) CGSize shadowOffset;
+
+/**
+ * The shadow blur radius
+ */
+@property (assign, nonatomic) CGFloat shadowBlur;
+
+@end
